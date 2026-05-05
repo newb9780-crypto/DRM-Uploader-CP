d002e3d4ab9b782c345e3854d3f8e98ac91fc6d2
newb9780-crypto <newb9780@gmail.com>
2026-05-05 12:26:58 +0530

    Update oldmain.py

--- oldmain.py
+++ oldmain.py
@@ -32,7 +32,7 @@ from base64 import b64encode, b64decode
 
 
 credit ="😎𝖘cᾰ𝗺𝗺ⲉ𝗿:)™~" 
-OWNER = int(os.environ.get("OWNER", 7081036509))      #replace your user id 
+OWNER = int(os.environ.get("OWNER", 1394117837))      #replace your user id 
 # try: 
 #     ADMINS=[] 
 #     for x in (os.environ.get("ADMINS", "7081036509 5702090016").split()):     ##replace your user id  (for multiple userid use white space) e.g. "123456 987456" <<- two userid ka example haa yee 
